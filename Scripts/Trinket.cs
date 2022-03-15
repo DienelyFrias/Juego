6f2b428948515925b06e7ac73966f5dc13baa34a
Dienely Meily Frias <100215795+DienelyFrias@users.noreply.github.com>
2022-03-15 11:21:11 -0400

    Add files via upload

---
+++ Scripts/Trinket.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trinket : MonoBehaviour
+{
+
+    public GameController gameController;
+    public AudioClip collected;
+
+    private AudioSource audioSource;
+
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            audioSource.PlayOneShot(collected);
+            Destroy(gameObject);
+            gameController.AddTrinket();
+        }
+    }
+}
